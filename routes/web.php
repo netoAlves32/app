54b4c57dbd45c159dfa31d69818768a2c1e3f788
netoAlves32 <harvin39@gmail.com>
2020-10-25 00:52:16 -0500

    hecho

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,23 @@
 <?php
 
-Route::get('/
+// satuFundacion.com  Route::get('/', function);
+
+// satuFundacion.com/contacto  Route::get('contacto/', function
+
+//Route::get('/',function(){
+//    return "Hola mi gente, que tal";
+//});
+
+//Route::pos();
+
+//Route::get('GaleriaInter/{saludo?}',function($saludo = "Nigeria"){
+//    return $saludo. "Galeria chocoana";
+//});
+
+Route::get('Contactanos',function(){
+    return "Nuestro número es 3146810822";
+})->name('Contactanos');
+
+Route::get('/',function(){
+    echo "<a href='".route('Contactanos')."'>Contacto</a><br>";
+});
